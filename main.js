e324d4bf4588d0e6740cc80e71f08e06400561b4
Azuko9 <53079264+Azuko9@users.noreply.github.com>
2024-10-07 22:09:06 +0200

    test gyro 0.4

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ if (window.DeviceOrientationEvent) {
   window.addEventListener('deviceorientation', function(event) {
     const { gamma, beta } = event; // gamma: gauche-droite, beta: haut-bas
     // Appliquez l'effet similaire à Tilt.js en fonction de ces valeurs
-    const element = document.querySelectorAll('.tilt');
+    const element = document.querySelector('.tilt');
     element.style.transform = `rotateY(${gamma*0.25}deg) rotateX(${beta*0.05}deg)`;
   });
 } else {
